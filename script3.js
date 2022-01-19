0ebc4daf09302487f432eadb227c2a9301e2a6ab
Tobi <example@gmail.com>
2022-01-19 15:23:17 +0600

    first commit

---
+++ script3.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const category = 'toys';
+
+console.log(`https://someurl.com${category}/5`);  /*метод интерполяции, при котором переход в категорию производится 
+с помощью косых ковычек ``*/
+
+const user = "Ivan";
+
+alert(`Привет, ${user}`);  
+
